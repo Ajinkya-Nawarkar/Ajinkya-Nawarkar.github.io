4361b96d9732efdb80740d77638744dc7f646a90
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2018-08-10 19:22:12 -0500

    Added particle-js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
